5d3943d9044059f6a920fda6d2bfd7ee8618a3b7
eedo_y <doup2001@naver.com>
2025-02-18 14:47:52 +0900

    chore : 프로젝트 기본 세팅

---
+++ src/main/java/com/proppay/platform/PropPayApplication.java
@@ -0,0 +1,13 @@
+package com.proppay.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropPayApplication.class, args);
+    }
+
+}
