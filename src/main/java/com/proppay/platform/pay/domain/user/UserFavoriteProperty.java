d7a395b7aec85d3885748b8edb8c59259bfcb405
eedo_y <doup2001@naver.com>
2025-02-19 18:20:49 +0900

    ✨feat : user 관련 유즈케이스 정의

---
+++ src/main/java/com/proppay/platform/pay/domain/user/UserFavoriteProperty.java
@@ -0,0 +1,14 @@
+package com.proppay.platform.pay.domain.user;
+
+import com.proppay.platform.pay.domain.BaseDomain;
+import com.proppay.platform.pay.domain.property.Property;
+
+public class UserFavoriteProperty extends BaseDomain {
+
+    private Long id;
+
+    private User user;
+
+    private Property property;
+
+}
