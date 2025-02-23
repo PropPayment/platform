93d85185d2062aa189507ffb1d9f873197c40096
eedo_y <doup2001@naver.com>
2025-02-23 12:17:29 +0900

    ✨feat : 매물 목록 조회시 dto 수정 - 시/군,구/동 나오도록

--- src/main/java/com/proppay/platform/pay/adapter/in/web/dto/PropertyListResponse.java
+++ src/main/java/com/proppay/platform/pay/adapter/in/web/dto/PropertyListResponse.java
@@ -1,6 +1,8 @@
 package com.proppay.platform.pay.adapter.in.web.dto;
 
+import com.proppay.platform.core.util.AddressParser;
 import com.proppay.platform.pay.domain.property.Property;
+import com.proppay.platform.pay.domain.property.PropertyAddress;
 import com.proppay.platform.pay.domain.property.PropertyType;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,17 +18,23 @@ public class PropertyListResponse {
     private long price;
     private String city;
     private String district;
+    private String dong;
     private long viewCount;
     private long likeCount;
 
     public static PropertyListResponse from(Property property) {
+
+        PropertyAddress address = AddressParser.parseAddress(property.getAddress().getStreetAddress());
+
+
         return PropertyListResponse.builder()
                 .id(property.getId())
                 .title(property.getSnippet().getAptName())
                 .type(property.getType())
                 .price(property.getPrice())
-                .city(property.getAddress().getCity())
-                .district(property.getAddress().getDistrict())
+                .city(address.getCity())
+                .district(address.getDistrict())
+                .dong(address.getDong())
                 .viewCount(property.getStatistic().getViewCount())
                 .likeCount(property.getStatistic().getLikeCount())
                 .build();
